d415f9da14e2c14966197d410bf5ef708f504555
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2023-01-31 21:50:17 +0330

    update pended changes

--- meetrest/entrypoint.sh
+++ meetrest/entrypoint.sh
@@ -2,7 +2,8 @@
 
 python manage.py migrate --no-input
 
-gunicorn meetrest.wsgi:application --bind 0.0.0.0:8000
-#python manage.py runserver 0.0.0.0:8000
 mkdir -p /app/static
 python manage.py collectstatic
+
+gunicorn meetrest.wsgi:application --bind 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
--- meetrest/meetrest/settings.py
+++ meetrest/meetrest/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-)5cl@k5dt7*e8)%1i(6m&8a-g5et7!#oer=gu1qh6^0t($qjy#")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = ['*']
 
@@ -39,16 +39,16 @@ SESSION_COOKIE_SAMESITE = 'None'
 
 from corsheaders.defaults import default_headers
 CORS_ALLOW_ALL_ORIGINS=  False
-#CORS_ALLOW_ALL_ORIGINS=  True
 CORS_ALLOW_CREDENTIALS = True
 
 #making sure CORS_ALLOW_HEADERS  is not "*"
 from corsheaders.defaults import default_headers
 CORS_ALLOW_HEADERS = list(default_headers) + ['Set-Cookie', 'x-xsrf-token', 'Content-Type', 'Content-Disposition']
-#CORS_ALLOW_HEADERS = ["*"]
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173','http://localhost:5173']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173','http://localhost:5173', 'https://res.vru.ac.ir']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173', 'https://res.vru.ac.ir']
 
 SESSION_COOKIE_HTTPONLY = False
 APPEND_SLASH = False
@@ -159,7 +159,7 @@ MEDIA_ROOT = 'meeting/assets'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
